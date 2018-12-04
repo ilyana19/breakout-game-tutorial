8a8e29137d6f13ecbfa2d0f6a15726dd6a096e94
ilyana19 <aries_2152@hotmail.com>
2018-12-04 18:40:16 -0500

    created paddle and made arrow key events to move it

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ document.addEventListener('DOMContentLoaded', function(){
   var dy = -2;
   var ballRadius = 10;
 
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width-paddleWidth)/2;
+
+  var rightPressed = false;
+  var leftPressed = false;
+
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
   function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -15,11 +25,34 @@ document.addEventListener('DOMContentLoaded', function(){
     ctx.closePath();
   }
 
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function keyDownHandler(e) {
+    if (e.keyCode === 39) {
+      rightPressed = true;
+    } else if (e.keyCode === 37) {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.keyCode === 39) {
+      rightPressed = false;
+    } else if (e.keyCode === 37) {
+      leftPressed = false;
+    }
+  }
+
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
+    drawPaddle();
 
     if (x + dx > (canvas.width-ballRadius) || x + dx < ballRadius) {
       dx = -dx;
@@ -28,6 +61,15 @@ document.addEventListener('DOMContentLoaded', function(){
     if (y+ dy > (canvas.height-ballRadius) || y + dy < ballRadius) {
       dy = -dy;
     }
+
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+      paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
   }
 
   setInterval(draw, 10);
