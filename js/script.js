042585ee6852e9fd9ad6f13b40c74ef2a5ba1283
ilyana19 <aries_2152@hotmail.com>
2018-12-04 18:44:50 -0500

    added gameover function

--- js/script.js
+++ js/script.js
@@ -58,8 +58,19 @@ document.addEventListener('DOMContentLoaded', function(){
       dx = -dx;
     }
 
-    if (y+ dy > (canvas.height-ballRadius) || y + dy < ballRadius) {
+    // if (y+ dy > (canvas.height-ballRadius) || y + dy < ballRadius) {
+    //   dy = -dy;
+    // }
+
+    if (y + dy < ballRadius) {
       dy = -dy;
+    } else if (y + dy > canvas.height-ballRadius) {
+      if (x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+      } else {
+        alert("GAME OVER");
+        document.location.reload();
+      }
     }
 
     if (rightPressed && paddleX < canvas.width-paddleWidth) {
