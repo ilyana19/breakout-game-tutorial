955cc458bfed2d526e3b39df1c29bd333450a182
ilyana19 <aries_2152@hotmail.com>
2018-12-04 19:01:21 -0500

    added collision

--- js/script.js
+++ js/script.js
@@ -14,6 +14,22 @@ document.addEventListener('DOMContentLoaded', function(){
   var rightPressed = false;
   var leftPressed = false;
 
+  var brickRowCount = 3;
+  var brickColumnCount = 5;
+  var brickWidth = 75;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 30;
+  var brickOffsetLeft = 30;
+
+  var bricks = [];
+  for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+      bricks[c][r] = {x: 0, y: 0, status: 1};
+    }
+  }
+
   document.addEventListener("keydown", keyDownHandler, false);
   document.addEventListener("keyup", keyUpHandler, false);
 
@@ -41,6 +57,24 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   }
 
+  function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+        if (bricks[c][r].status === 1) {
+          var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+          var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+
   function keyUpHandler(e) {
     if (e.keyCode === 39) {
       rightPressed = false;
@@ -49,18 +83,31 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   }
 
+  function collisionDetection() {
+    for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+        var b = bricks[c][r];
+
+        if (b.status === 1) {
+          if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+            dy = -dy;
+            b.status = 0;
+          }
+        }
+      }
+    }
+  }
+
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     if (x + dx > (canvas.width-ballRadius) || x + dx < ballRadius) {
       dx = -dx;
     }
-
-    // if (y+ dy > (canvas.height-ballRadius) || y + dy < ballRadius) {
-    //   dy = -dy;
-    // }
 
     if (y + dy < ballRadius) {
       dy = -dy;
